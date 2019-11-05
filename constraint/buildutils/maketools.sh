6540031de8dc6fc7623539902ffbf1f8e4bd93e8
Max Smythe <smythe@google.com>
2019-11-05 13:22:22 -0800

    More output from buildutils
    Signed-off-by: Max Smythe <smythe@google.com>

--- constraint/buildutils/maketools.sh
+++ constraint/buildutils/maketools.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
+echo 'Starting tool build...'
+
+echo 'controller-gen'
 go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.1.8
+
+echo 'client-gen'
 go get k8s.io/code-generator/cmd/client-gen@93d7507fc8ffb4c860e4dd6f9a51a5e1985aebcd
+
+echo 'deepcopy-gen'
 go get k8s.io/code-generator/cmd/deepcopy-gen@93d7507fc8ffb4c860e4dd6f9a51a5e1985aebcd
+
+echo 'conversion-gen'
 go get k8s.io/code-generator/cmd/conversion-gen@93d7507fc8ffb4c860e4dd6f9a51a5e1985aebcd
 
 mkdir /.output/buildutil
