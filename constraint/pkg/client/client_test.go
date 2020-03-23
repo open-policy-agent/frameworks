2b4fead1eabc5b63621b723c9237a19b78822f4e
Max Smythe <smythe@google.com>
2020-03-23 16:57:19 -0700

    Allow users to get/delete templates by name only
    Signed-off-by: Max Smythe <smythe@google.com>

--- constraint/pkg/client/client_test.go
+++ constraint/pkg/client/client_test.go
@@ -465,12 +465,16 @@ func TestRemoveTemplate(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			r, err := c.RemoveTemplate(context.Background(), tt.Template)
+			r, err := c.AddTemplate(context.Background(), tt.Template)
 			if err != nil && !tt.ErrorExpected {
 				t.Errorf("err = %v; want nil", err)
 			}
 			if err == nil && tt.ErrorExpected {
 				t.Error("err = nil; want non-nil")
+			}
+			r, err = c.RemoveTemplate(context.Background(), tt.Template)
+			if err != nil {
+				t.Errorf("err = %v; want nil", err)
 			}
 			expectedCount := 0
 			expectedHandled := make(map[string]bool)
@@ -483,6 +487,200 @@ func TestRemoveTemplate(t *testing.T) {
 			}
 			if !reflect.DeepEqual(r.Handled, expectedHandled) {
 				t.Errorf("r.Handled = %v; want %v", r.Handled, expectedHandled)
+			}
+		})
+	}
+}
+
+func TestRemoveTemplateByNameOnly(t *testing.T) {
+	badRegoTempl := createTemplate(name("fake"), crdNames("Fake"), targets("h1"))
+	badRegoTempl.Spec.Targets[0].Rego = "asd{"
+	tc := []struct {
+		Name          string
+		Handler       TargetHandler
+		Template      *templates.ConstraintTemplate
+		ErrorExpected bool
+	}{
+		{
+			Name:          "Good Template",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h1")),
+			ErrorExpected: false,
+		},
+		{
+			Name:          "Unknown Target",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h2")),
+			ErrorExpected: true,
+		},
+		{
+			Name:          "Bad CRD",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), targets("h1")),
+			ErrorExpected: true,
+		},
+	}
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+			d := local.New()
+			b, err := NewBackend(Driver(d))
+			if err != nil {
+				t.Fatalf("Could not create backend: %s", err)
+			}
+			c, err := b.NewClient(Targets(tt.Handler))
+			if err != nil {
+				t.Fatal(err)
+			}
+			r, err := c.AddTemplate(context.Background(), tt.Template)
+			if err != nil && !tt.ErrorExpected {
+				t.Errorf("err = %v; want nil", err)
+			}
+			if err == nil && tt.ErrorExpected {
+				t.Error("err = nil; want non-nil")
+			}
+			sparseTemplate := &templates.ConstraintTemplate{}
+			sparseTemplate.Name = tt.Template.Name
+			r, err = c.RemoveTemplate(context.Background(), sparseTemplate)
+			if err != nil {
+				t.Errorf("err = %v; want nil", err)
+			}
+			expectedCount := 0
+			expectedHandled := make(map[string]bool)
+			if !tt.ErrorExpected {
+				expectedCount = 1
+				expectedHandled = map[string]bool{"h1": true}
+			}
+			if r.HandledCount() != expectedCount {
+				t.Errorf("HandledCount() = %v; want %v", r.HandledCount(), expectedCount)
+			}
+			if !reflect.DeepEqual(r.Handled, expectedHandled) {
+				t.Errorf("r.Handled = %v; want %v", r.Handled, expectedHandled)
+			}
+		})
+	}
+}
+
+func TestGetTemplate(t *testing.T) {
+	badRegoTempl := createTemplate(name("fake"), crdNames("Fake"), targets("h1"))
+	badRegoTempl.Spec.Targets[0].Rego = "asd{"
+	tc := []struct {
+		Name          string
+		Handler       TargetHandler
+		Template      *templates.ConstraintTemplate
+		ErrorExpected bool
+	}{
+		{
+			Name:          "Good Template",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h1")),
+			ErrorExpected: false,
+		},
+		{
+			Name:          "Unknown Target",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h2")),
+			ErrorExpected: true,
+		},
+		{
+			Name:          "Bad CRD",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), targets("h1")),
+			ErrorExpected: true,
+		},
+	}
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+			d := local.New()
+			b, err := NewBackend(Driver(d))
+			if err != nil {
+				t.Fatalf("Could not create backend: %s", err)
+			}
+			c, err := b.NewClient(Targets(tt.Handler))
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = c.AddTemplate(context.Background(), tt.Template)
+			if err != nil && !tt.ErrorExpected {
+				t.Errorf("err = %v; want nil", err)
+			}
+			if err == nil && tt.ErrorExpected {
+				t.Error("err = nil; want non-nil")
+			}
+			tmpl, err := c.GetTemplate(context.Background(), tt.Template)
+			if err != nil && !tt.ErrorExpected {
+				t.Errorf("err = %v; want nil", err)
+			}
+			if err == nil && tt.ErrorExpected {
+				t.Error("err = nil; want non-nil")
+			}
+			if !tt.ErrorExpected {
+				if !reflect.DeepEqual(tmpl, tt.Template) {
+					t.Error("Stored and retrieved template differ")
+				}
+			}
+		})
+	}
+}
+
+func TestGetTemplateByNameOnly(t *testing.T) {
+	badRegoTempl := createTemplate(name("fake"), crdNames("Fake"), targets("h1"))
+	badRegoTempl.Spec.Targets[0].Rego = "asd{"
+	tc := []struct {
+		Name          string
+		Handler       TargetHandler
+		Template      *templates.ConstraintTemplate
+		ErrorExpected bool
+	}{
+		{
+			Name:          "Good Template",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h1")),
+			ErrorExpected: false,
+		},
+		{
+			Name:          "Unknown Target",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), crdNames("Fake"), targets("h2")),
+			ErrorExpected: true,
+		},
+		{
+			Name:          "Bad CRD",
+			Handler:       &badHandler{Name: "h1", HasLib: true},
+			Template:      createTemplate(name("fake"), targets("h1")),
+			ErrorExpected: true,
+		},
+	}
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+			d := local.New()
+			b, err := NewBackend(Driver(d))
+			if err != nil {
+				t.Fatalf("Could not create backend: %s", err)
+			}
+			c, err := b.NewClient(Targets(tt.Handler))
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = c.AddTemplate(context.Background(), tt.Template)
+			if err != nil && !tt.ErrorExpected {
+				t.Errorf("err = %v; want nil", err)
+			}
+			if err == nil && tt.ErrorExpected {
+				t.Error("err = nil; want non-nil")
+			}
+			sparseTemplate := &templates.ConstraintTemplate{}
+			sparseTemplate.Name = tt.Template.Name
+			tmpl, err := c.GetTemplate(context.Background(), sparseTemplate)
+			if err != nil && !tt.ErrorExpected {
+				t.Errorf("err = %v; want nil", err)
+			}
+			if err == nil && tt.ErrorExpected {
+				t.Error("err = nil; want non-nil")
+			}
+			if !tt.ErrorExpected {
+				if !reflect.DeepEqual(tmpl, tt.Template) {
+					t.Error("Stored and retrieved template differ")
+				}
 			}
 		})
 	}
