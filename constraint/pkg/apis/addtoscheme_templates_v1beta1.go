f6d289d0a8dc10a2312ccbcc392711f82dccca3e
Sertac Ozercan <sozercan@gmail.com>
2021-01-20 15:22:21 -0800

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- constraint/pkg/apis/addtoscheme_templates_v1beta1.go
+++ constraint/pkg/apis/addtoscheme_templates_v1beta1.go
@@ -15,7 +15,9 @@ limitations under the License.
 
 package apis
 
+import "github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1beta1"
+
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, AddToSchemes...)
+	AddToSchemes = append(AddToSchemes, v1beta1.AddToScheme)
 }
