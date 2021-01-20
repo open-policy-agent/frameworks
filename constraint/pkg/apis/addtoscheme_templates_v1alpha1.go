4a9f20258fe851cf0c95a90d939e92ad03a74f5f
Sertac Ozercan <sozercan@gmail.com>
2021-01-20 09:55:44 -0800

    fix addtoscheme
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- constraint/pkg/apis/addtoscheme_templates_v1alpha1.go
+++ constraint/pkg/apis/addtoscheme_templates_v1alpha1.go
@@ -15,11 +15,7 @@ limitations under the License.
 
 package apis
 
-import (
-	"github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1alpha1"
-)
-
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, v1alpha1.AddToSchemes...)
+	AddToSchemes = append(AddToSchemes, AddToSchemes...)
 }
