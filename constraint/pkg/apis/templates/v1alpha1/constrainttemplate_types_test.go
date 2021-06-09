ae8a149f8be50e292a6a0a40971bcc572adb939a
juliankatz <juliankatz@google.com>
2021-06-09 11:24:15 -0700

    Added extra test cases for when LegacySchema=false
    Signed-off-by: juliankatz <juliankatz@google.com>

--- constraint/pkg/apis/templates/v1alpha1/constrainttemplate_types_test.go
+++ constraint/pkg/apis/templates/v1alpha1/constrainttemplate_types_test.go
@@ -139,7 +139,7 @@ func TestValidationVersionConversionAndTransformation(t *testing.T) {
 		error bool
 	}{
 		{
-			name: "Two deep properties",
+			name: "Two deep properties, LegacySchema=true",
 			v: &Validation{
 				LegacySchema: true,
 				OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
@@ -175,6 +175,53 @@ func TestValidationVersionConversionAndTransformation(t *testing.T) {
 								Schema: &apiextensions.JSONSchemaProps{
 									Type:                   "object",
 									XPreserveUnknownFields: &trueBool,
+									Properties: map[string]apiextensions.JSONSchemaProps{
+										"key":          {Type: "string"},
+										"allowedRegex": {Type: "string"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			error: false,
+		},
+		{
+			name: "Two deep properties, LegacySchema=false",
+			v: &Validation{
+				LegacySchema: false,
+				OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+					Properties: map[string]apiextensionsv1.JSONSchemaProps{
+						"message": {
+							Type: "string",
+						},
+						"labels": {
+							Type: "array",
+							Items: &apiextensionsv1.JSONSchemaPropsOrArray{
+								Schema: &apiextensionsv1.JSONSchemaProps{
+									Type: "object",
+									Properties: map[string]apiextensionsv1.JSONSchemaProps{
+										"key":          {Type: "string"},
+										"allowedRegex": {Type: "string"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			exp: &templates.Validation{
+				OpenAPIV3Schema: &apiextensions.JSONSchemaProps{
+					Properties: map[string]apiextensions.JSONSchemaProps{
+						"message": {
+							Type: "string",
+						},
+						"labels": {
+							Type: "array",
+							Items: &apiextensions.JSONSchemaPropsOrArray{
+								Schema: &apiextensions.JSONSchemaProps{
+									Type: "object",
 									Properties: map[string]apiextensions.JSONSchemaProps{
 										"key":          {Type: "string"},
 										"allowedRegex": {Type: "string"},
