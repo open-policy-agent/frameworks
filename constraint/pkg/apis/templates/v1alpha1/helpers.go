8b29d7828b8add41cc089620c967494dd8f535c9
juliankatz <juliankatz@google.com>
2021-08-03 16:32:58 -0700

    Generate schemes for ToVersionless functions
    Signed-off-by: juliankatz <juliankatz@google.com>

--- constraint/pkg/apis/templates/v1alpha1/helpers.go
+++ constraint/pkg/apis/templates/v1alpha1/helpers.go
@@ -1,21 +1,16 @@
 package v1alpha1
 
 import (
-	"fmt"
-
 	"github.com/open-policy-agent/frameworks/constraint/pkg/core/templates"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // ToVersionless runs defaulting functions and then converts the ConstraintTemplate to the
 // versionless api representation
-func (versioned *ConstraintTemplate) ToVersionless(scheme *runtime.Scheme) (*templates.ConstraintTemplate, error) {
-	if scheme == nil {
-		return nil, fmt.Errorf("Cannot convert using nil scheme")
-	}
-
-	if !scheme.IsVersionRegistered(SchemeGroupVersion) {
-		return nil, fmt.Errorf("GroupVersion '%v/%v' not registered in scheme", SchemeGroupVersion.Group, SchemeGroupVersion.Version)
+func (versioned *ConstraintTemplate) ToVersionless() (*templates.ConstraintTemplate, error) {
+	scheme := runtime.NewScheme()
+	if err := AddToScheme(scheme); err != nil {
+		return nil, err
 	}
 
 	versionedCopy := versioned.DeepCopy()
