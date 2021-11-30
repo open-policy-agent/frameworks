0087992f539f45f52a7a7cf0f3861fda93df0c84
Will Beason <willbeason@google.com>
2021-11-30 10:59:41 -0800

    Make Client E2E tests more flexible (#150)
    * Rename e2e_tests to e2e_tests
    
    Signed-off-by: Will Beason <willbeason@google.com>
    
    * Make tests more flexible
    
    Refactor tests so that:
    
    - it is possible to specify rego and libraries per-test
    - all tests use their own Client
    - the Probe type is no longer necessary
    
    Signed-off-by: Will Beason <willbeason@google.com>
    
    * Use cmp.Diff to check for response differences
    
    This makes test output on failure more consistent and clearer.
    
    Use per-test Context instead of one shared between all tests. This
    removes another avenue of test crosstalk and allows us to test things
    like behavior with cancellable contexts.
    
    Make response order more, but not completely, deterministic. We don't
    need response order to be more deterministic for tests than what I've
    put here, and as-is will make the output of reviewing objects more
    consistent. There are common edge cases where ordering is randomized,
    but this can be fully fixed later.
    
    Comment out the contents of TestRemoteClientE2E. This test is currently
    skipped until we have a working remote to connect the remote Driver to.
    I've added clarifying comments to explain what we'll want once this
    exists.
    
    Signed-off-by: Will Beason <willbeason@google.com>
    
    * Make reviewer-suggested changes
    
    Signed-off-by: Will Beason <willbeason@google.com>

--- constraint/tests/remote_test.go
+++ constraint/tests/remote_test.go
@@ -2,28 +2,34 @@ package tests
 
 import (
 	"testing"
-
-	"github.com/open-policy-agent/frameworks/constraint/pkg/client"
-	"github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/remote"
 )
 
 func TestRemoteClientE2E(t *testing.T) {
 	t.Skip("test does not work yet")
 
-	d, err := remote.New(remote.URL("http://localhost:8181"), remote.Tracing(false))
-	if err != nil {
-		t.Fatal(err)
-	}
-	p, err := client.NewProbe(d)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for name, f := range p.TestFuncs() {
-		t.Run(name, func(t *testing.T) {
-			if err := f(); err != nil {
-				t.Fatal(err)
-			}
-		})
-	}
+	// This code no longer compiles, but is preserved so the original intent is
+	// preserved once we have a working remote to test the Client against.
+	//
+	// Most likely what we'd want is a TestMain function which can toggle whether
+	// the local or remote Driver is used for E2E tests. If we want these to
+	// execute by default when "go test" is run (including in CI) then we'd want
+	// to call m.Run() twice in TestMain after the local driver tests run
+	// (and pass).
+	//
+	// d, err := remote.New(remote.URL("http://localhost:8181"), remote.Tracing(false))
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// p, err := client.NewProbe(d)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	//
+	// for name, f := range p.TestFuncs() {
+	// 	t.Run(name, func(t *testing.T) {
+	// 		if err := f(); err != nil {
+	// 			t.Fatal(err)
+	// 		}
+	// 	})
+	// }
 }
