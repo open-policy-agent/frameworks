bbb44686e5a8a8e1a547318c96f0af15b283e60f
Becky Huang <beckyhd@google.com>
2022-01-21 16:54:08 -0800

    No error returns for removing a non-matching constraint (#170)
    Signed-off-by: Becky Huang <beckyhd@google.com>

--- constraint/pkg/client/drivers/local/local.go
+++ constraint/pkg/client/drivers/local/local.go
@@ -616,7 +616,9 @@ func (d *Driver) AddConstraint(ctx context.Context, constraint *unstructured.Uns
 func (d *Driver) RemoveConstraint(ctx context.Context, constraint *unstructured.Unstructured) error {
 	handlers, err := d.getTargetHandlers(constraint)
 	if err != nil {
-		return err
+		if !errors.Is(err, ErrMissingConstraintTemplate) {
+			return err
+		}
 	}
 	for _, target := range handlers {
 		relPath, err := createConstraintPath(target, constraint)
