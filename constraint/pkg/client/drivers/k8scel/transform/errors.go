a96b0e395449dffddeb67b436affee9f73256b2b
Max Smythe <smythe@google.com>
2024-01-06 02:18:16 +0000

    Create VAP binding objects; update semantic equals to include labels
    Signed-off-by: Max Smythe <smythe@google.com>

---
+++ constraint/pkg/client/drivers/k8scel/transform/errors.go
@@ -0,0 +1,7 @@
+package transform
+
+import "errors"
+
+var (
+	ErrBadEnforcementAction = errors.New("invalid enforcement action")
+)
--- constraint/pkg/client/drivers/k8scel/transform/make_vap_objects.go
+++ constraint/pkg/client/drivers/k8scel/transform/make_vap_objects.go
@@ -2,12 +2,16 @@ package transform
 
 import (
 	"fmt"
+	"strings"
 
+	apiconstraints "github.com/open-policy-agent/frameworks/constraint/pkg/apis/constraints"
 	templatesv1beta1 "github.com/open-policy-agent/frameworks/constraint/pkg/apis/templates/v1beta1"
 	"github.com/open-policy-agent/frameworks/constraint/pkg/client/drivers/k8scel/schema"
 	"github.com/open-policy-agent/frameworks/constraint/pkg/core/templates"
 	admissionregistrationv1alpha1 "k8s.io/api/admissionregistration/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TemplateToPolicyDefinition(template *templates.ConstraintTemplate) (*admissionregistrationv1alpha1.ValidatingAdmissionPolicy, error) {
@@ -56,4 +60,61 @@ func TemplateToPolicyDefinition(template *templates.ConstraintTemplate) (*admiss
 		},
 	}
 	return policy, nil
+}
+
+func ConstraintToBinding(constraint *unstructured.Unstructured) (*admissionregistrationv1alpha1.ValidatingAdmissionPolicyBinding, error) {
+	enforcementActionStr, err := apiconstraints.GetEnforcementAction(constraint)
+	if err != nil {
+		return nil, err
+	}
+
+	var enforcementAction admissionregistrationv1alpha1.ValidationAction
+	switch enforcementActionStr {
+	case apiconstraints.EnforcementActionDeny:
+		enforcementAction = admissionregistrationv1alpha1.Deny
+	case "warn":
+		enforcementAction = admissionregistrationv1alpha1.Warn
+	default:
+		return nil, fmt.Errorf("%w: unrecognized enforcement action %s, must be `warn` or `deny`", ErrBadEnforcementAction, enforcementActionStr)
+	}
+
+	binding := &admissionregistrationv1alpha1.ValidatingAdmissionPolicyBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: fmt.Sprintf("g8r-%s", constraint.GetName()),
+		},
+		Spec: admissionregistrationv1alpha1.ValidatingAdmissionPolicyBindingSpec{
+			PolicyName: fmt.Sprintf("g8r-%s", strings.ToLower(constraint.GetKind())),
+			ParamRef: &admissionregistrationv1alpha1.ParamRef{
+				Name: constraint.GetName(),
+			},
+			MatchResources:    &admissionregistrationv1alpha1.MatchResources{},
+			ValidationActions: []admissionregistrationv1alpha1.ValidationAction{enforcementAction},
+		},
+	}
+	objectSelectorMap, found, err := unstructured.NestedMap(constraint.Object, "spec", "match", "labelSelector")
+	if err != nil {
+		return nil, err
+	}
+	var objectSelector *metav1.LabelSelector
+	if found {
+		objectSelector = &metav1.LabelSelector{}
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(objectSelectorMap, objectSelector); err != nil {
+			return nil, err
+		}
+		binding.Spec.MatchResources.ObjectSelector = objectSelector
+	}
+
+	namespaceSelectorMap, found, err := unstructured.NestedMap(constraint.Object, "spec", "match", "namespaceSelector")
+	if err != nil {
+		return nil, err
+	}
+	var namespaceSelector *metav1.LabelSelector
+	if found {
+		namespaceSelector = &metav1.LabelSelector{}
+		if err := runtime.DefaultUnstructuredConverter.FromUnstructured(namespaceSelectorMap, namespaceSelector); err != nil {
+			return nil, err
+		}
+		binding.Spec.MatchResources.NamespaceSelector = namespaceSelector
+	}
+	return binding, nil
 }
