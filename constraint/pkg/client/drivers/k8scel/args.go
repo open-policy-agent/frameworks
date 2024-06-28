e4a32bacf009609dbbe5f182a8d01ccbe179f67a
Jaydip Gabani <gabanijaydip@gmail.com>
2024-06-28 21:30:11 +0000

    removing generate default
    Signed-off-by: Jaydip Gabani <gabanijaydip@gmail.com>

--- constraint/pkg/client/drivers/k8scel/args.go
+++ constraint/pkg/client/drivers/k8scel/args.go
@@ -11,17 +11,3 @@ func GatherStats() Arg {
 		return nil
 	}
 }
-
-// VAPGenerationDefault sets the expected default
-// value for the generateVAP field.
-// If no value is provided, VAP generation
-// is presumed to be disabled and the engine will
-// validate ALL policies. Otherwise, the engine
-// will only validate policies not expected to be
-// enforced via VAP.
-func VAPGenerationDefault(d bool) Arg {
-	return func(driver *Driver) error {
-		driver.generateVAPDefault = d
-		return nil
-	}
-}
