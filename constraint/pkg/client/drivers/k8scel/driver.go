e7364230db85b82b12da3d44cc634667bd49ebb7
Jaydip Gabani <gabanijaydip@gmail.com>
2024-08-02 21:30:36 +0000

    adding strictCost const
    Signed-off-by: Jaydip Gabani <gabanijaydip@gmail.com>

--- constraint/pkg/client/drivers/k8scel/driver.go
+++ constraint/pkg/client/drivers/k8scel/driver.go
@@ -85,7 +85,7 @@ func (d *Driver) AddTemplate(_ context.Context, ct *templates.ConstraintTemplate
 	}
 	vapVars = append(vapVars, vapVarsSuffix...)
 	// Defaulting to true in MustBaseEnvSet to enforce strict cost calculation for CEL, following k8s.
-	filterCompiler, err := cel.NewCompositedCompiler(environment.MustBaseEnvSet(environment.DefaultCompatibilityVersion(), true))
+	filterCompiler, err := cel.NewCompositedCompiler(environment.MustBaseEnvSet(environment.DefaultCompatibilityVersion(), transform.StrictCost))
 	if err != nil {
 		return err
 	}
