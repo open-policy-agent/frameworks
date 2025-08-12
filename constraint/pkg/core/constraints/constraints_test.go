23aa33f572dca86c9b93b0b3d188d82962b12448
Noah Reisch <noahreisch4@gmail.com>
2025-08-12 19:48:06 -0400

    Change SemanticEqual to include annotations for constraint caching (#595)
    * Change SemanticEqual to include annotations for constraint^Caching
    
    Signed-off-by: Noah Reisch <noahreisch4@gmail.com>
    
    * nit test name
    
    Signed-off-by: Noah Reisch <noahreisch4@gmail.com>
    
    * fix lint
    
    Signed-off-by: Noah Reisch <noahreisch4@gmail.com>
    
    ---------
    
    Signed-off-by: Noah Reisch <noahreisch4@gmail.com>
    (cherry picked from commit 1e1e2926f82fd8579549cbafe48527d5ce3bceab)
    Signed-off-by: Noah Reisch <noahreisch4@gmail.com>

--- constraint/pkg/core/constraints/constraints_test.go
+++ constraint/pkg/core/constraints/constraints_test.go
@@ -6,7 +6,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-func TestSemanticEqual(t *testing.T) {
+func TestSemanticEqualWithLabelsAndAnnotations(t *testing.T) {
 	testCases := []struct {
 		name string
 		c1   *unstructured.Unstructured
@@ -157,18 +157,272 @@ func TestSemanticEqual(t *testing.T) {
 			},
 			want: false,
 		},
+		{
+			name: "equal annotations",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "equal annotations, keys in different order",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit1": "enabled1", "audit2": "enabled2"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit2": "enabled2", "audit1": "enabled1"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "different annotation values",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit": "disabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "different annotation keys",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit2": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit1": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "one has annotations, other doesn't",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{"annotations": map[string]interface{}{"audit": "enabled"}},
+					"spec":     map[string]interface{}{"a": "b"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"spec": map[string]interface{}{"a": "b"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "equal labels and annotations",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod", "team": "security"},
+						"annotations": map[string]interface{}{"audit": "enabled", "export": "true"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod", "team": "security"},
+						"annotations": map[string]interface{}{"audit": "enabled", "export": "true"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "different labels, same annotations",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod"},
+						"annotations": map[string]interface{}{"audit": "enabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "staging"},
+						"annotations": map[string]interface{}{"audit": "enabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "same labels, different annotations",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod"},
+						"annotations": map[string]interface{}{"audit": "enabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod"},
+						"annotations": map[string]interface{}{"audit": "disabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "nil labels, equal annotations",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"annotations": map[string]interface{}{"export": "true"},
+					},
+					"spec": map[string]interface{}{"rule": "allow"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"annotations": map[string]interface{}{"export": "true"},
+					},
+					"spec": map[string]interface{}{"rule": "allow"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "nil annotations, equal labels",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels": map[string]interface{}{"team": "security"},
+					},
+					"spec": map[string]interface{}{"rule": "allow"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels": map[string]interface{}{"team": "security"},
+					},
+					"spec": map[string]interface{}{"rule": "allow"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "ignores status field",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod"},
+						"annotations": map[string]interface{}{"audit": "enabled"},
+					},
+					"spec":   map[string]interface{}{"rule": "deny-all"},
+					"status": map[string]interface{}{"violations": 5},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"labels":      map[string]interface{}{"env": "prod"},
+						"annotations": map[string]interface{}{"audit": "enabled"},
+					},
+					"spec":   map[string]interface{}{"rule": "deny-all"},
+					"status": map[string]interface{}{"violations": 10},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "ignores other metadata fields",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":            "test-constraint-1",
+						"namespace":       "default",
+						"resourceVersion": "123",
+						"uid":             "abc-123",
+						"labels":          map[string]interface{}{"env": "prod"},
+						"annotations":     map[string]interface{}{"audit": "enabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{
+						"name":            "test-constraint-2",
+						"namespace":       "kube-system",
+						"resourceVersion": "456",
+						"uid":             "def-456",
+						"labels":          map[string]interface{}{"env": "prod"},
+						"annotations":     map[string]interface{}{"audit": "enabled"},
+					},
+					"spec": map[string]interface{}{"rule": "deny-all"},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "empty labels and annotations are treated as nil",
+			c1: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"metadata": map[string]interface{}{},
+					"spec":     map[string]interface{}{"rule": "allow"},
+				},
+			},
+			c2: &unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"spec": map[string]interface{}{"rule": "allow"},
+				},
+			},
+			want: true,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := SemanticEqual(tc.c1, tc.c2)
+			got := SemanticEqualWithLabelsAndAnnotations(tc.c1, tc.c2)
 			if got != tc.want {
-				t.Fatalf("got SemanticEqual(c1, c2) = %v, want %v", got, tc.want)
+				t.Fatalf("got SemanticEqualWithLabelsAndAnnotations(c1, c2) = %v, want %v", got, tc.want)
 			}
 
-			got2 := SemanticEqual(tc.c2, tc.c1)
+			got2 := SemanticEqualWithLabelsAndAnnotations(tc.c2, tc.c1)
 			if got2 != tc.want {
-				t.Fatalf("got SemanticEqual(c2, c1) = %v, want %v", got, tc.want)
+				t.Fatalf("got SemanticEqualWithLabelsAndAnnotations(c2, c1) = %v, want %v", got2, tc.want)
 			}
 		})
 	}
